c21ec80fc36f6b0d2465f689aeafff3de1744aa2
sky777light <sky777light@gmail.com>
2017-03-31 18:33:51 +0300

    first commit

--- server/config/index.js
+++ server/config/index.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     env: process.env.NODE_ENV,
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 3007,
     mongoose: {
         uri: "mongodb://localhost/oxivisual"
     },
